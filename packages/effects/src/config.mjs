e9368dd46592f4df2368adf86e0dc11457c515da
Takuro Fukamizu <takuro.f.1987@gmail.com>
2023-11-04 03:02:11 +0900

    replace python code to node one

---
+++ packages/effects/src/config.mjs
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const envpath = path.resolve(__dirname, '..', '.env');
+console.log("envpath", envpath);
+dotenv.config(envpath);
+console.log("env", process.env) // remove this 
+
+export const Configs = {
+    voicevoxUrl: process.env.VOICEVOX_URL
+};
