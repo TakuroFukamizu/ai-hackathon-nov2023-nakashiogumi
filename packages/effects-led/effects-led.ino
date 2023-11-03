edc5395bebe583950f5e81011bc830cd5901c890
Takuro Fukamizu <takuro.f.1987@gmail.com>
2023-11-03 21:42:58 +0900

    hw fixed

--- packages/effects-led/effects-led.ino
+++ packages/effects-led/effects-led.ino
@@ -38,6 +38,10 @@ uint8_t blinkToggle = 0;
 void turnOffAll();
 void ledTest();
 void setIndexsToStart();
+
+void challenger1Speaking();
+void challenger2Speaking();
+void judgeTinking();
 void challenger1Win();
 void challenger2Win();
 
@@ -68,37 +72,44 @@ LED Mapping
  87-121(35): Challenger2 Circle
 */
 void loop() {
-    Serial.println("led test");
-    // ledTest();
-    
+    // Serial.println("loop");
+    if (Serial.available()) {
+        int inByte = Serial.read();
+        Serial.print(inByte);
+        // NOTE: 面倒なのでとりあえずASCIIコードのまま処理する
+        inByte = inByte - 48;
+        if (inByte<0 || inByte>5) {
+            Serial.println(": invalid data");
+        } else {
+            Serial.println(": accepted");
+            effectMode = inByte;
+        }
+    }
+
     switch(effectMode) {
-        case 0: // turn off all
+        case EFFECT_MODE_NONE: // turn off all
             turnOffAll();
             break;
-        case 1: // challenger1 speaking
+        case EFFECT_MODE_C1_SPK: // challenger1 speaking
+            challenger1Speaking();
             break;
-        case 2: // challenger2 speaking
+        case EFFECT_MODE_C2_SPK: // challenger2 speaking
+            challenger2Speaking();
             break;
-        case 3: // judge thinking
+        case EFFECT_MODE_JUDGE_INPROGRESS: // judge thinking
+            judgeTinking();
             break;
-        case 4: // judge selected challenger1
+        case EFFECT_MODE_C1_WIN: // judge selected challenger1
             challenger1Win();
             break;
-        case 5: // judge selected challenger2
+        case EFFECT_MODE_C2_WIN: // judge selected challenger2
             challenger2Win();
             break;
     }
     delay(30);
 }
 
-// --------
-
-void turnOffAll() {
-    for(int i=0;i<NUM_LEDS;i++) {
-        leds[i] = CRGB::Black;
-    }
-    FastLED.show();
-}
+// ----------------------------------------------------------------
 
 void ledTest() {
     turnOffAll();
@@ -144,7 +155,14 @@ void ledTest() {
     turnOffAll();
 }
 
+// ----------------------------------------------------------------
 
+void turnOffAll() {
+    for(int i=0;i<NUM_LEDS;i++) {
+        leds[i] = CRGB::Black;
+    }
+    FastLED.show();
+}
 
 void setIndexsToStart()
 {
@@ -153,6 +171,71 @@ void setIndexsToStart()
     judgeCircleIndex = 43; // 43..77
     challenger2BarIndex = 78; // 78..85
     challenger2CircleIndex = 86; // 86..120
+}
+
+// ----------------------------------------------------------------
+
+void challenger1Speaking() {
+    if (prevEffectMode != EFFECT_MODE_C1_SPK) {
+        turnOffAll(); // clear all
+        setIndexsToStart(); // clear all index
+        blinkToggle = 0;
+        prevEffectMode = effectMode;
+    }
+
+    // Challenger1 Circle 
+    for(int a=0;a<35;a++) {
+        leds[a] = CRGB::Black; // clear all
+    }
+    leds[challenger1CircleIndex] = CRGB::Red;
+    challenger1CircleIndex++; // 0..34
+    if (challenger1CircleIndex>34) {
+        challenger1CircleIndex = 0;
+    }
+    FastLED.show();
+    delay(50);
+}
+
+void challenger2Speaking() {
+    if (prevEffectMode != EFFECT_MODE_C2_SPK) {
+        turnOffAll(); // clear all
+        setIndexsToStart(); // clear all index
+        blinkToggle = 0;
+        prevEffectMode = effectMode;
+    }
+
+    // Challenger2 Circle 
+    for(int e=86;e<121;e++) {
+        leds[e] = CRGB::Black; // clear all
+    }
+    leds[challenger2CircleIndex] = CRGB::Red;
+    challenger2CircleIndex++; // 86..120
+    if (challenger2CircleIndex>120) {
+        challenger2CircleIndex = 86;
+    }
+    FastLED.show();
+    delay(50);
+}
+
+void judgeTinking() {
+    if (prevEffectMode != EFFECT_MODE_JUDGE_INPROGRESS) {
+        turnOffAll(); // clear all
+        setIndexsToStart(); // clear all index
+        blinkToggle = 0;
+        prevEffectMode = effectMode;
+    }
+
+    // Challenger2 Circle 
+    for(int c=43;c<78;c++) {
+        leds[c] = CRGB::Black; // clear all
+    }
+    leds[judgeCircleIndex] = CRGB::Green;
+    judgeCircleIndex++; // 43..77
+    if (judgeCircleIndex>77) {
+        judgeCircleIndex = 43;
+    }
+    FastLED.show();
+    delay(50);
 }
 
 void challenger1Win() {
