907f055a45bb5f040deb6421e8797e6aa6f24c39
mamiyR <kataoka@aiqlab.com>
2023-11-04 08:08:02 +0900

    修正

--- packages/software/main.py
+++ packages/software/main.py
@@ -1,10 +1,6 @@
-from aiohttp import web
 import asyncio
-from bottle import route, run, request, post
-import requests
+import aiohttp
 import json
-import time, threading
-import concurrent.futures
 import challengerA
 import challengerB
 import Judge
@@ -13,69 +9,135 @@ import Judge
 
 # NOTE: 音声featch path param:特定キャラの名前、 body param:text
 # NOTE: judgmentのfetch対象になる予定
-
 # 
+
+
+    
 async def handle_suggestion(current_step):
-    # 各キャラクターからの提案を並行して取得
-    suggestion_results = await asyncio.gather(
-        challengerA.Character.suggestion({'current_step': current_step}), 
-        challengerB.Character.suggestion({'current_step': current_step}),
-    )
+    async with aiohttp.ClientSession() as session:
+        try:
+            # 各キャラクターからの提案を並行して取得
+            suggestion_results = await asyncio.gather(
+                challengerA.Character.suggestion({'current_step': current_step}),
+                challengerB.Character.suggestion({'current_step': current_step})
+            )
+            print('🌟🌟')
+            print(suggestion_results)
+            
+            # 提案結果をjudgmentに送り判定を受け取る
+            suggestions = [{'message': res['result_msg'], 'current_step': current_step, 'fromType': res['fromType'],}
+                        for i, res in enumerate(suggestion_results)]
+            print('🌟')
+            print(suggestions)
+            
+            
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+            json_payloadA= json.dumps({'text':suggestions[0]['result_msg'], 'name':suggestions[0]['name'] })
+            json_payloadB= json.dumps({'text':suggestions[1]['result_msg'], 'name':suggestions[1]['name'] })
+            # それぞれ提案する音声発話
+            userA = suggestions[0]['fromType']
+            userB = suggestions[1]['fromType']
+            await session.post(f'http://localhost:8080/challenger/{userA}/speak/', headers=headers, data=json_payloadA, timeout=180)
+            await session.post(f'http://localhost:8080/challenger/{userB}/speak/', headers=headers, data=json_payloadB, timeout=180)
+            
+            # 判定者に提案内容を送る
+            judgeResult = Judge.Judge.judgment(suggestions)
+            # 選ばれた挑戦者
+            wonChallenger = 'a' if judgeResult[result][0] == True else 'b'
 
-    # 提案結果をjudgmentに送り判定を受け取る
-    suggestions = [{'message': res['result_msg'], 'current_step': current_step, 'fromType': 'A' if i == 0 else 'B'}
-                   for i, res in enumerate(suggestion_results)]
-    judgeResult = Judge.Judge.judgment(suggestions)
+            # 判定結果を音声出力
+            json_payload = json.dumps({'text': judgeResult['result_msg']})
+            
+            # 判定結果発話　音声再生し、LED演出を行う。 再生が終わったらレスポンスする。
+            # NOTE: 音声出力完了を待つため、timeoutは3分とする
+            if current_step < 3:
+                r = await session.post(
+                    "http://localhost:8080/judge/speak/", 
+                    headers=headers, 
+                    data=json_payload, 
+                    timeout=aiohttp.ClientTimeout(total=180))
+            else:
+                r  = await session.post(f'''http://localhost:8080/judge/select/{wonChallenger}''', headers=headers, data=json_payload, timeout=180)
+                session.post("http://localhost:8080/session/reset", headers=headers, timeout=aiohttp.ClientTimeout(total=180))
+            # r = await asyncio.gather(
+            #     requests.post("http://localhost:8080/judge/speak/", data=payload, timeout=180),
+            #     requests.post("http://localhost:8080/judge/speak/", data=payload, timeout=180)
+            # )
+            # await vv_request_speech(judgeResult['result_msg'])
+            print(r)
+            if r.status == 200:
+                # Read the response body if needed
+                # response_body = await response.text()
+                pass
+        except asyncio.TimeoutError:
+            # Handle request timeout here
+            print("Request timed out")
+        except aiohttp.ClientError as e:
+            # Handle other aiohttp-specific client errors here
+            print(f"Client error: {e}")
 
-    # 判定結果を音声出力
-    payload = {'text': judgeResult['result_msg']}
-    # NOTE: 以下は参照にあたってeffects/libs直下を立ち上げておく必要あり。音声出力完了を待つため、timeoutは3分とする
-    r = await requests.post("http://localhost:8080/challenger/", data=payload, timeout=180)
-    # await vv_request_speech(judgeResult['result_msg'])
-    print(r)
-
-    # 勝者の提案を次のステップに進める
-    if True in judgeResult['result']:
-        winner_index = judgeResult['result'].index(True)
-        if winner_index == 0:
-            return 'A', current_step + 1
+        # 勝者の提案を次のステップに進める
+        if True in judgeResult['result']:
+            winner_index = judgeResult['result'].index(True)
+            if winner_index == 0:
+                return 'A', current_step + 1
+            else:
+                return 'B', current_step + 1
         else:
-            return 'B', current_step + 1
-    else:
-        # どちらも勝者がいない場合は次のステップに進まない
-        return None, current_step
+            # どちらも勝者がいない場合は次のステップに進まない
+            return None, current_step
 
 async def handle_reaction(character, current_step):
-    if character == 'A':
-        response = await challengerA.Character.reaction({'current_step': current_step})
-    else:
-        response = await challengerB.Character.reaction({'current_step': current_step})
+    async with aiohttp.ClientSession() as session:
+        try:
+            if character == 'A':
+                response = await challengerA.Character.reaction({'current_step': current_step})
+            else:
+                response = await challengerB.Character.reaction({'current_step': current_step})
 
-    # 反応を音声出力
-    # await vv_request_speech(response['result_msg'])
+            # 反応を音声出力
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+            await session.post("http://localhost:8080/judge/speak/", headers={headers}, data={'Content-Type': 'application/x-www-form-urlencoded'}, timeout=aiohttp.ClientTimeout(total=180))
+            # await vv_request_speech(response['result_msg'])
+        except asyncio.TimeoutError:
+            # Handle request timeout here
+            print("Request timed out")
+        except aiohttp.ClientError as e:
+            # Handle other aiohttp-specific client errors here
+            print(f"Client error: {e}")
 
 async def main():
-    current_step = 0
-    winner = None
+    async with aiohttp.ClientSession() as session:
+        try:
+            current_step = 0
+            winner = None
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-    while current_step <= 3:
-        if winner is not None:
-            # 前のステップの勝者から始める
-            winner_suggestion = suggestionA if winner == 'A' else suggestionB
-            res = await winner_suggestion({'current_step': current_step})
-            await vv_request_speech(res['result_msg'])
-        winner, current_step = await handle_suggestion(current_step)
-        if winner is None:
-            # 両方のreactionを呼び出す
-            await asyncio.gather(
-                handle_reaction('A', current_step),
-                handle_reaction('B', current_step)
-            )
-            break  # 反応後にループ終了
+            while current_step <= 3:
+                if winner is not None:
+                    # 前のステップの勝者から始める
+                    winner_suggestion = challengerA.Character.suggestion if winner == 'A' else challengerB.Character.suggestion
+                    res = await winner_suggestion({'current_step': current_step})
+                    # await vv_request_speech(res['result_msg'])
+                    await session.post(f'http://localhost:8080/challenger/{winner}/speak/', headers=headers, data=payload, timeout=aiohttp.ClientTimeout(total=180))
+                winner, current_step = await handle_suggestion(current_step)
+                if winner is None:
+                    # 両方のreactionを呼び出す
+                    await asyncio.gather(
+                        handle_reaction('A', current_step),
+                        handle_reaction('B', current_step)
+                    )
+                    break  # 反応後にループ終了
+            
+        except asyncio.TimeoutError:
+            # Handle request timeout here
+            print("Request timed out")
+        except aiohttp.ClientError as e:
+            # Handle other aiohttp-specific client errors here
+            print(f"Client error: {e}")
 
-    print("処理を終了します。")  # すべての処理が終わったことを出力
-    
-    # TODO: M5Stackに投げる
+        print("処理を終了します。")  # すべての処理が終わったことを出力
+        
     
 async def start_background_tasks(app):
     app['main'] = asyncio.create_task(main())
