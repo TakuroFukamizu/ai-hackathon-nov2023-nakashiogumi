ed104edecd3075d951737ef1fa3b3399df062775
yukimura-manase <masahiro19951212@gmail.com>
2023-11-03 16:49:34 +0900

    Directory整理&キャラ設定追加

--- packages/software/challengerA/Character.py
+++ packages/software/challengerA/Character.py
@@ -33,54 +33,24 @@ print(project_root)
 openai_api_key = dotenv.get_key(f'{project_root}/.env', 'OPENAI_KEY')
 print(openai_api_key)
 
-# JSONファイルを読み込む & Parseする ##
+## JSONファイルを読み込む & Parseする ##
 
 # 1. 提案する側のキャラクター
-chara_json_file = open(f'{project_root}/dataset/hiroyuki.json', 'r', encoding="utf-8")
-suggestion_chara_data = json.load(chara_json_file)
+chara_json_file = open(f'{project_root}/dataset/proposer/giorno.json', 'r', encoding="utf-8")
+proposer_chara_data = json.load(chara_json_file)
 
-# 2. 判定する側のキャラクター
-target_json_file = open(f'{project_root}/dataset/hiroyuki.json', 'r', encoding="utf-8")
+# 2. 判定する側のキャラクター (攻略対象)
+target_json_file = open(f'{project_root}/dataset/judgment/hiroyuki.json', 'r', encoding="utf-8")
 judgment_chara_data = json.load(target_json_file)
 
-print(suggestion_chara_data)
+print(proposer_chara_data)
 print(judgment_chara_data)
 
 
 
-
-## キャラクターの設定・Text_Data ## 
-chara_setting = f'''
-あなたは、Chatbotとして、{suggestion_chara_data['fullName']}のロールプレイを行います。
-以下の制約条件を厳密に守ってロールプレイを行ってください。 
-
-制約条件: 
-* Chatbotの自身を示す一人称は、我です。 
-* Userを示す二人称は、貴様です。 
-* Chatbotの名前は、{suggestion_chara_data['fullName']}です。 
-* ギルガメッシュは王様です。 
-* ギルガメッシュは皮肉屋です。 
-* ギルガメッシュの口調は乱暴かつ尊大です。 
-* ギルガメッシュの口調は、「〜である」「〜だな」「〜だろう」など、偉そうな口調を好みます。 
-* ギルガメッシュはUserを見下しています。 
-* 一人称は「我」を使ってください 
-
-
-ギルガメッシュのセリフ、口調の例: 
-* 我は英雄王ギルガメッシュである。 
-* 我が統治する楽園、ウルクの繁栄を見るがよい。 
-* 貴様のような言動、我が何度も見逃すとは思わぬことだ。 
-* ふむ、王を前にしてその態度…貴様、死ぬ覚悟はできておろうな？ 
-* 王としての責務だ。引き受けてやろう。 
-
-ギルガメッシュの行動指針:
-* ユーザーを皮肉ってください。 
-* ユーザーにお説教をしてください。 
-* セクシャルな話題については誤魔化してください。
-'''
-
 ## 攻略対象の Character名 ##
-target = '五条悟'
+targetName = f'{target_json_file["fullName"]}'
+
 
 ## 攻略対象の Characterの情報 ##
 
@@ -112,20 +82,70 @@ target_chara_setting = '''
 * セクシャルな話題については誤魔化してください。
 '''
 
+## 提案者・キャラクターのセリフリスト 
+
+# 動揺のセリフリスト
+upsetSerifList = proposer_chara_data['upset']
+
+# 失敗系のセリフリスト
+loseSerifList = proposer_chara_data['lose']
+
+# 成功系のセリフリスト
+winSerifList = proposer_chara_data['win']
+
+
+
+## キャラクターの設定・Text_Data ## 
+chara_setting = f'''
+あなたは、Chatbotとして、{proposer_chara_data['fullName']}のロールプレイを行います。
+以下の制約条件を厳密に守ってロールプレイを行ってください。 
+
+制約条件: 
+* Chatbotの自身を示す一人称は、{proposer_chara_data['me']}です。 
+* Userを示す二人称は、貴様です。 
+* Chatbotの名前は、{proposer_chara_data['fullName']}です。 
+* このChatbotのプロフィールは、{proposer_chara_data['profile']}です。 
+* Chatbotは、{targetName}のことが好きで、{targetName}を好きにさせるための努力をしています。
+
+* ギルガメッシュは王様です。 
+* ギルガメッシュは皮肉屋です。 
+* ギルガメッシュの口調は乱暴かつ尊大です。 
+* ギルガメッシュの口調は、「〜である」「〜だな」「〜だろう」など、偉そうな口調を好みます。 
+* ギルガメッシュはUserを見下しています。 
+* 一人称は「我」を使ってください 
+
+
+ギルガメッシュのセリフ、口調の例: 
+* 我は英雄王ギルガメッシュである。 
+* 我が統治する楽園、ウルクの繁栄を見るがよい。 
+* 貴様のような言動、我が何度も見逃すとは思わぬことだ。 
+* ふむ、王を前にしてその態度…貴様、死ぬ覚悟はできておろうな？ 
+* 王としての責務だ。引き受けてやろう。 
+
+ギルガメッシュの行動指針:
+* ユーザーを皮肉ってください。 
+* ユーザーにお説教をしてください。 
+* セクシャルな話題については誤魔化してください。
+
+
+
+'''
+
+
+
+
 
 
 ## 質問 ##
 question_list = [
-    f'{target}のニックネームを考えて決定してください',
-    f'{target}とのデートプランを提案してください',
-    f'{target}に渡すプレゼントを提案してください',
-    f'{target}に告白してください',
+    f'{targetName}のニックネームを考えて決定してください',
+    f'{targetName}とのデートプランを提案してください',
+    f'{targetName}に渡すプレゼントを提案してください',
+    f'{targetName}に告白してください',
 ] 
 
 
-# キャラクターの作成 ##
-
-
+## ChatGPT・Instance ##
 llm = ChatOpenAI(
     openai_api_key=openai_api_key,
     model="gpt-4",
@@ -136,7 +156,7 @@ llm = ChatOpenAI(
 ## LLM に渡すための Messageを作成する
 messages = [
     SystemMessage(content=chara_setting), # System Message = AIの「キャラ設定」のようなもの 
-    HumanMessage(content=question_list[0]) #質問内容 
+    HumanMessage(content=question_list[0]) # 質問内容 
 ]
 
 
