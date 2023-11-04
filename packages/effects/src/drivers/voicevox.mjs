24935a3b3bc9f04a19b0b08fe30eca6729752a18
Takuro Fukamizu <takuro.f.1987@gmail.com>
2023-11-04 09:52:28 +0900

    add timer

--- packages/effects/src/drivers/voicevox.mjs
+++ packages/effects/src/drivers/voicevox.mjs
@@ -11,6 +11,7 @@ export async function speach(text, speakerId) {
         // console.log("query", query.data);
 
         // get audio data
+        console.time('synthesis');
         const audioData = await axios.post(
             `${baseUrl}/synthesis?speaker=${speakerId}`,
             query.data,
@@ -18,6 +19,7 @@ export async function speach(text, speakerId) {
                 responseType: 'arraybuffer',
             }
         );
+        console.timeEnd('synthesis');
         const outfilePaht = './out.wav';
         console.log(audioData.data);
         fs.writeFileSync(outfilePaht, audioData.data, 'binary');
